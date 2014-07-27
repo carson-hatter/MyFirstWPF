306f093e2027d4aa9cc76f8dc17de79856eb1de3
carson-hatter <carson.hatter@yahoo.com>
2014-07-27 12:04:14 -0500

    First Sync
    Need to learn:
    - Editting a table programatically in C#
    - SQL in WPF

---
+++ TablePage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyFirstWPF
+{
+    /// <summary>
+    /// Interaction logic for TablePage.xaml
+    /// </summary>
+    public partial class TablePage : Page
+    {
+        public TablePage()
+        {
+            InitializeComponent();
+        }
+
+        private void newEntry(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void reviseEntry(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void deleteEntry(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void quitProgram(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
